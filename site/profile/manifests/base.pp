5868d1281c68c466cfd409a2351dad381754d89f
Chris Barker <cbarker@puppet.com>
2018-03-18 18:46:52 -0700

    add new hiera layout and modules

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -3,4 +3,9 @@ class profile::base {
   #the base profile should include component modules that will be on all nodes
   include ntp
 
+  package { 'aws-sdk':
+    ensure => present,
+    provider => puppet_gem,
+  }
+
 }
